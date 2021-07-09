fb082db2acb0ba3cd7d0eb4c71ad4582bb55c1e0
Danilo Castro <danilogcastro@hotmail.com>
2021-07-09 19:05:19 -0300

    defined models associations

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,7 +1,23 @@
 class Api::V1::OrdersController < ApplicationController
   def index
+    @orders = Order.all
+    render json: orders, status: 200
   end
 
   def show
+    @order = Order.find(params[:id])
+    if @order
+      render json: @order, status: 200
+    else
+      render json: {error: "Order not found"}
+    end
+  end
+
+  def create
+  end
+
+  private
+
+  def order_params
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,5 @@
 class Order < ApplicationRecord
   belongs_to :customer
+  has_many :payments
+  has_and_belongs_to_many :items
 end
