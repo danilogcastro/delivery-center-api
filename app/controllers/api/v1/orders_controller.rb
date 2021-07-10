38a88396cacebb30bf1168e4d3019c2bc18bc0ef
Danilo Castro <danilogcastro@hotmail.com>
2021-07-10 12:44:33 -0300

    create method in orders controller

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -18,10 +18,44 @@ class Api::V1::OrdersController < ApplicationController
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.build_customer
+    @order.items.build
+    @order.payments.build
+    if @order.save
+      render json: @order, include: { customer: {except: [:id, :created_at, :updated_at ]},
+                                      items: {except: [:id, :created_at, :updated_at]},
+                                      payments: {except: [:id, :created_at, :updated_at]}
+                                    },
+                                      except: [:id, :customer_id, :created_at, :updated_at ], status: 200
+    else
+      render json: {error: "Error creating order"}
+    end
   end
 
   private
 
   def order_params
+    params.require(:order).permit(:external_code, 
+                                  :store_id, 
+                                  :sub_total,
+                                  :delivery_fee,
+                                  :total_shipping,
+                                  :total,
+                                  :country,
+                                  :state,
+                                  :city,
+                                  :district,
+                                  :street,
+                                  :complement,
+                                  :latitude,
+                                  :longitude,
+                                  :dt_order_create,
+                                  :postal_code,
+                                  :number,
+                                  customer_attributes: [:external_code, :name, :email, :contact],
+                                  items_attributes: [:external_code, :name, :price, :quantity, :total],
+                                  payments_attributes: [:kind, :value]
+                                )
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ApplicationRecord
   belongs_to :customer
   has_many :payments
   has_and_belongs_to_many :items
+
+  accepts_nested_attributes_for :customer, :items, :payments
 end
